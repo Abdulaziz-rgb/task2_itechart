60d3a8946753b1de0bc755e63b898ca4f2003da8
Abdulaziz-rgb <shokirovabdulaziz19@gmail.com>
2022-10-28 18:49:13 +0500

    added Task 2 Solution

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace tesk2_itechart
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            // Configuring log4net
+            BasicConfigurator.Configure();
+            
+            // Creating a Logger
+            ILog logger =  LogManager.GetLogger(typeof(Program));
+            
+            // Initiliazing Chrome Driver
+            var webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+            
+            // Setting Page Load Timeout
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(25);
+            
+            // Navigating to a page
+            webDriver.Navigate().GoToUrl($"https://www.google.com/");
+            
+            // Writing the search query
+            var searchField = webDriver.FindElement(By.Name("q"));
+            searchField.SendKeys("Hello World");
+            
+            // Clicking Google Search button
+            var searchBtn = webDriver.FindElement(By.Name("btnK"));
+            searchBtn.Click();
+            
+            // Logging the endresult
+            logger.Info(webDriver.Title);
+            
+        }
+    }
+}
